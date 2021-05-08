205a4bff939aa3344975f30f4fa3197a4a7f7e21
theodore <cuixin1974@stnts.com>
2021-05-08 10:55:45 +0800

    修改入口

--- src/test/main.java
+++ src/test/main.java
@@ -1,39 +1,36 @@
-package com.day01.demo1;
+package test;
+
 import Observer.CurrentConditionsDisplay;
 import Observer.StatisticsDisplay;
-import Observer.Subject;
 import Observer.WeatherData;
+
 import Strategy.FlyRocketPowered;
 import Strategy.RedHatDuck;
 
-class main {
+
+public class main {
     public static void main(String[] args) {
+
         /* Strategy */
-/*
+        /*
         RedHatDuck redHatDuck = new RedHatDuck();
         redHatDuck.swim();
         redHatDuck.performFly();
         redHatDuck.setFlyBehavior(new FlyRocketPowered());
         redHatDuck.performFly();
         redHatDuck.performQuack();
-*/
-
+        */
 
         /* Observer push */
         WeatherData weatherData = new WeatherData();
         StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
         CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
         weatherData.setMeasurements(123,123,123);
-        weatherData.setMeasurements(231,231,412);
-
         /* Observer B */
-
-
-
-
-
     }
 
-}
 
+
+
+}
 
