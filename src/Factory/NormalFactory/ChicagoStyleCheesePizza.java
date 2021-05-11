43de5e2d2558fcdbece9e310460d2a676d651fe6
theodore <cuixin1974@stnts.com>
2021-05-11 11:59:31 +0800

    Factory

---
+++ src/Factory/NormalFactory/ChicagoStyleCheesePizza.java
@@ -0,0 +1,19 @@
+package Factory.NormalFactory;
+
+public class ChicagoStyleCheesePizza extends Pizza{
+    public ChicagoStyleCheesePizza() {
+        name = "Chicago Style Deep Dish Cheese Pizza";
+        dough = "Extra Thick Crust Dough";
+        sauce = "Plum Tomato Sauce";
+
+        toppings.add("Shredded Mozzarella Cheese");
+    }
+
+    void bake() {
+        System.out.println("Bake for 15 minutes at 400");
+    }
+
+    void cut() {
+        System.out.println("Cutting the pizza into square slices");
+    }
+}
