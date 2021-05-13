1af32f50bdbaed486e2acd7d1780b858594d47ac
theodore <cuixin1974@stnts.com>
2021-05-13 11:06:03 +0800

    Singleton

--- src/Singleton/Singleton.java
+++ src/Singleton/Singleton.java
@@ -1,14 +1,29 @@
 package Singleton;
 
 public class Singleton {
-    private static Singleton uniqueInstance;
+    private volatile static Singleton uniqueInstance;
+    // 急切实例化
+    // private static Singleton uniqueInstance = new Singleton();
 
     private Singleton(){}
 
     public static Singleton getInstance(){
         if (uniqueInstance == null){
-            uniqueInstance = new Singleton();
+            synchronized (Singleton.class){
+                if (uniqueInstance == null){
+                    uniqueInstance = new Singleton();
+                }
+            }
         }
         return uniqueInstance;
     }
+
+   /* synchronized避免初始化的时候多个线程的同步安全问题
+      但之后的获取实例过程无需同步等待
+    public synchronized static Singleton getInstance(){
+        if (uniqueInstance == null){
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }*/
 }
